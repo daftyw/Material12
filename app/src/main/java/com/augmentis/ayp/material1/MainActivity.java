86d14f43a46564cac84df99777d73602946f0c88
Rawin Ngamloet <rawin.n@augmentis.biz>
2016-09-08 16:13:39 +0700

    init commit

---
+++ app/src/main/java/com/augmentis/ayp/material1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.augmentis.ayp.material1;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        //getWindow().setExitTransition(new Explode());
+        //getWindow().setEnterTransition(new Explode());
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_on_main);
+
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        if(ab != null) {
+            ab.setIcon(R.drawable.ic_ab_icon);
+        }
+
+    }
+}
